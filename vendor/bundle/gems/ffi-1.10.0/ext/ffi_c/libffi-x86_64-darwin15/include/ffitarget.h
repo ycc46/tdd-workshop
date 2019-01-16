b9ebf932948044782005b542efb30cccaccf0301
刘宏庆-rubydev <liuhongqing@sudiyi.cn>
2019-01-16 20:38:30 +0800

    spec init

--- vendor/bundle/gems/ffi-1.10.0/ext/ffi_c/libffi-x86_64-darwin15/include/ffitarget.h
+++
@@ -1,1 +0,0 @@
-/Users/ane/projects/tdd-workshop/vendor/bundle/gems/ffi-1.10.0/ext/ffi_c/libffi/src/x86/ffitarget.h
--- vendor/bundle/gems/mock_redis-0.19.0/lib/mock_redis/version.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-# Defines the gem version.
-class MockRedis
-  VERSION = '0.19.0'.freeze
-end
