f9942dcf8e4efe17a5278be6827ae071a652655e
ycc <1493617238@qq.com>
2019-01-23 21:02:30 +0800

    add controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  namespace :api, defult: { format: :json } do
+  namespace :api, defaults: { format: :json } do
     scope module: :v1 do
-
+      resources :users, only: [:show]
     end
   end
 
