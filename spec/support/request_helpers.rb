19496ea2a4072d88088a2d601cf276067517f6d2
ycc <1493617238@qq.com>
2019-02-19 14:10:22 +0800

    update

--- spec/support/request_helpers.rb
+++ spec/support/request_helpers.rb
@@ -1,7 +1,7 @@
 module Request
   module JsonHelpers
     def json_response
-      @json_response ||= JSON.parse(response.body, symbolize_name: true)
+      @json_response ||= JSON.parse(response.body, symbolize_names: true)
     end
   end
 
